feee6b4a2ad721b7ebbeb2ef3e1caf5961c53542
lesuuda <lesuudaljamani@gmail.com>
2024-07-04 20:18:05 +0300

    some mandatory tasks

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" This module contains a function that calculates the floor of a float """
+
+
+def floor(n: float) -> int:
+    """ This function calculates the floor of a float """
+    return int(n)
---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" This module contains a function that defines variables """
+
+
+
+""" defines variables """
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
