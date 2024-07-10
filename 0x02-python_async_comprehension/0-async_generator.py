646a669fafffcb60dbd86c417214eca4070c2185
lesuuda <lesuudaljamani@gmail.com>
2024-07-10 09:29:57 +0300

    fix: correction for number 0 by using generator instead of asyncgen

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -3,11 +3,11 @@
 
 
 import random
-from typing import AsyncGenerator
+from typing import Generator
 import asyncio
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """ Async generator """
     for _ in range(10):
         await asyncio.sleep(1)
