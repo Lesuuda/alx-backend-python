1e6dc9f4caefd8a3eb91a7e707ea242cc50c910e
lesuuda <lesuudaljamani@gmail.com>
2024-07-10 09:33:22 +0300

    fixed annotation for return type of number 1 to List

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -2,9 +2,10 @@
 """ Async comprehension """
 
 
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> list[float]:
+async def async_comprehension() -> List[float]:
     """ Async comprehension """
     return [i async for i in async_generator()]
