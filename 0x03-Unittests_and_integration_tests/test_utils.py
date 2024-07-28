4e4c523fe63cd43c740b438c90038f3621f41a4e
lesuuda <lesuudaljamani@gmail.com>
2024-07-28 22:59:09 +0300

    core: updated test_utils.py to include tests for exception

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -21,3 +21,13 @@ class TestAccessNestedMap(unittest.TestCase):
         """ test case for test_access_nested_map
         """
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ["a"], KeyError),
+        ({"a": 1}, ["a", "b"], KeyError)
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, expected):
+        """tests for access_nested_map exception
+        """
+        with self.assertRaises(expected):
+            access_nested_map(nested_map, path)
