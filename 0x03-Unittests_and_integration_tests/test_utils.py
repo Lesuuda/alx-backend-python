5c6c4311af464177258ed5acd9dd493c004d6934
lesuuda <lesuudaljamani@gmail.com>
2024-07-28 22:51:13 +0300

    core: added test for access_nested_map function

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+unittests to parameteize a unit test
+"""
+
+
+import unittest
+from parameterized import parameterized
+from utils import access_nested_map
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """TestAccessNestedMap Class
+    """
+    @parameterized.expand([
+        ({"a": 1}, ["a"], 1),
+        ({"a": {"b": 2}}, ["a"], {"b": 2}),
+        ({"a": {"b": 2}}, ["a", "b"], 2)
+    ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        """ test case for test_access_nested_map
+        """
+        self.assertEqual(access_nested_map(nested_map, path), expected)
